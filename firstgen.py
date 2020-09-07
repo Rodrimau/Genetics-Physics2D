b15293c5cebe9e5ea2178eb4568d04b687287e51
rmaurino <rmaurino@practia.global>
2020-09-06 23:41:02 -0300

    First Generation

--- firstgen.py
+++ firstgen.py
@@ -10,15 +10,15 @@ import random
 from PIL import Image
 import random
 from itertools import combinations
-
+import statistics
 
 # Constant
 # b0 = space.static_body
-size = w, h = 1800, 600
+size = w, h = 2700, 800
 # Colors
 GRAY = (220, 220, 220)
 RED = (255, 0, 0)
-BLUE = (50, 50, 200)
+BLUE = (200, 50, 200)
 GREEN = (0, 255, 0)
 class App:
     def __init__(self, size, elements=None):
@@ -36,18 +36,22 @@ class App:
         self.gravity = False
         self.startTime = time.time()
         self.seconds = 0
-        self.springs = elements
+        self.springs = None
         self.fps = 60
+
 
     def run(self):
         Box(self.space)
-        # Subject(self.space)
+        subject1 = subject(self.space)
+        subject1.create()
         while self.running:
             for event in pygame.event.get():
                 self.do_event(event)
-            self.draw()
+            
+            self.draw(subject1)
             dt = self.clock.tick(self.fps)
             self.space.step(dt/1000)
+
         pygame.quit()
 
     def do_event(self, event):
@@ -63,13 +67,10 @@ class App:
             if event.key in keys:
                 v = Vec2d(keys[event.key]) 
                 if self.active_shape != None:
-                        # self.active_shape.body.position += v
                         v.normalized()
-                        print(v)
-                        self.active_shape.body.apply_impulse_at_world_point(v*5000)            
+                        self.active_shape.body.apply_impulse_at_world_point(v*5000,point=self.active_shape.body.position)     
 
             if event.key == K_h:
-                self.gravity = not self.gravity
                 if self.gravity:
                     self.space.gravity = 0, 0
                 else:
@@ -77,19 +78,15 @@ class App:
 
             if event.key == K_r:
                 self.space.remove(self.space.constraints + self.space.shapes)
-                subject.create(self.space)   
                 self.startTime = time.time()             
                 self.run()
 
-            # if event.key == K_f:
-            #     self.fps += 10          
-            # if event.key == K_s:
-            #     self.fps -= 10    
-
             if event.key == K_c:
                 p = from_pygame(pygame.mouse.get_pos(), self.screen)
-                Circle(p, radius=20)
-
+                new_circle = Circle(p,space = self.space,color = RED, radius=20).shape
+                dist, info = new_circle.point_query(p)
+                if dist < 0:
+                    self.active_shape = new_circle
 
         elif event.type == MOUSEBUTTONDOWN:
             p = from_pygame(event.pos, self.screen)
@@ -107,8 +104,8 @@ class App:
                     else:
                         self.selected_shapes = [] 
                         
-        # elif event.type == MOUSEMOTION:
-        #     self.p = event.pos
+        elif event.type == MOUSEMOTION:
+            self.p = event.pos
 
         elif event.type == MOUSEBUTTONUP:
             if self.pulling:
@@ -120,7 +117,7 @@ class App:
                 b.apply_impulse_at_local_point(impulse)
             pass
 
-    def draw(self):
+    def draw(self, subject):
         self.screen.fill(GRAY)
         self.space.debug_draw(self.draw_options)
         # Texto:
@@ -129,10 +126,16 @@ class App:
         self.screen.blit(img, (800,20))
         prev_seconds = self.seconds
         self.seconds = ((time.time() - self.startTime))
-        # for x in self.springs:
-        # # if prev_seconds!= self.seconds:
-        #     x.joint.rest_length += 10*math.sin(5*prev_seconds)
-        # print(spring.joint.rest_length)
+        # Its Alive
+        pygame.draw.lines(self.screen ,RED ,  False,(Vec2d(subject.positionStart,0) ,Vec2d(subject.positionStart,900)) , 5)
+        springs = subject.springList
+        for x in springs:
+            if prev_seconds!= self.seconds:
+                x.spring.joint.rest_length += 1*math.sin(2*prev_seconds) 
+        print(x.spring.joint.rest_length)
+        subject.fit()
+        pygame.draw.lines(self.screen ,GREEN ,  False,(Vec2d(subject.positionEnd,0) ,Vec2d(subject.positionEnd,900)) , 5)
+
         pygame.display.update()
 
 
@@ -148,7 +151,7 @@ class SimpleMotor:
         space.add(self.joint)
 
 class SpringJoint:
-    def __init__(self, b1, b2, space, a1 = (0, 0), a2 =(0, 0), length = 100,stif =100,damp=10 ):
+    def __init__(self, b1, b2, space, a1 = (0, 0), a2 =(0, 0), length = 100,stif =100,damp=10,grooved=0 ):
         self.joint = pymunk.constraint.DampedSpring(b1, b2, a1, a2, length, stif, damp)
         space.add(self.joint)
 
@@ -159,11 +162,17 @@ class GrooveJoint:
         space.add(self.joint)
 
 class TSpringJoint:
-    def __init__(self, b1, b2, space, a1 = Vec2d(0, 0), a2 =Vec2d(0, 0), length = 100,stif =100,damp=10,max_length=0.01,grooved =True ):
+    def __init__(self, b1, b2, space, a1 = Vec2d(0, 0), a2 =Vec2d(0, 0), length = 100,stif =100,damp=1000,max_length=1.5,grooved =True ):
         
         if grooved:
-            self.groove1 = GrooveJoint(b1, b2, space,a1=a1,a2=max_length*length*(b2.position - b1.position),anchor_b=(0,0))
-            self.groove2 = GrooveJoint(b2, b1, space,a2=a2,a1=max_length*length*(b1.position- b2.position),anchor_b=(0,0))
+            groovedWich = 3
+            if groovedWich==1:
+                self.groove1 = GrooveJoint(b1, b2, space,a1=a1,a2=max_length*length*(b2.position - b1.position).normalized(),anchor_b=(0,0))
+            if groovedWich==2:
+                self.groove2 = GrooveJoint(b2, b1, space,a2=a2,a1=max_length*length*(b1.position- b2.position).normalized(),anchor_b=(0,0))
+            if groovedWich==3:
+                self.groove2 = GrooveJoint(b2, b1, space,a2=a2,a1=max_length*length*(b1.position- b2.position).normalized(),anchor_b=(0,0))
+                self.groove1 = GrooveJoint(b1, b2, space,a1=a1,a2=max_length*length*(b2.position - b1.position).normalized(),anchor_b=(0,0))
         self.spring = SpringJoint(b1,b2, space,length=length)
 
 
@@ -171,50 +180,52 @@ class TSpringJoint:
 
 class Circle:
     id = 0
-    def __init__(self, pos, space , radius=30,color=GRAY,friction=0.5,mass=0):
+    def __init__(self, pos, space , radius=30,color=GRAY,friction=0.5,mass=1):
         self.id = (Circle.id + 1)
         self.body = pymunk.Body(mass=mass)
         self.body.position = pos
-        shape = pymunk.Circle(self.body, radius)
-        shape.density = 1
-        shape.color = color
-        shape.friction = friction
-        shape.elasticity = 1
-        space.add(self.body, shape)
+        self.shape = pymunk.Circle(self.body, radius)
+        self.shape.density = 0.1
+        self.shape.color = color
+        self.shape.friction = friction
+        self.shape.elasticity = 1
+        space.add(self.body, self.shape)
 
 class Box:
-    def __init__(self, space, p0=(0, 0), p1=(w, h), d=4):
+    def __init__(self, space, p0=(0, 0), p1=(w, h), d=40):
         x0, y0 = p0
         x1, y1 = p1
         ps = [(x0, y0), (x1, y0), (x1, y1), (x0, y1)]
         for i in range(4):
             segment = pymunk.Segment(space.static_body, ps[i], ps[(i+1) % 4], d)
-            segment.elasticity = 0.5
-            segment.friction = 1
+            segment.elasticity = 0
+            segment.friction = 0.5
             space.add(segment)
 
 class Floor:
     def __init__(self, space , p0=(-200, 0), p1=(w+200,), d=10):
         segment = pymunk.Segment(space.static_body, p0 , p1, d)
         segment.elasticity = 0.5
-        segment.friction = 1
+        segment.friction = 0.5
         space.add(segment)
 
-class subject():
-
-    def __init__(self):
-        self.nodes = 2+random.randint(1,5)
+class subject:
+    def __init__(self, space ):
+        self.nodes = 2+random.randint(1,3)
         self.springs = random.randint((self.nodes)-1,sum(range(self.nodes)))
-
-    def create(self,space):
+        self.positionStart = 0
+        self.positionEnd = 0
+        self.fitness =  0
+        self.space = space
+        
+    def create(self):
         # Defino cantidad de Nodes
         nodesList =[]
         for _ in range(self.nodes):
-            nodesList.append(Circle((900+random.randint(0,200), 100+random.randint(0,200)),
-            color =(255,0,255), friction = 10*random.random(),
-            mass=random.randint(0,100),
-            radius=random.randint(10, 30),
-            space = space))
+            nodesList.append(Circle((500+random.randint(0,400), 100+random.randint(0,400)),
+            color =(200,100,255), friction = 100*random.random(),
+            radius=random.randint(20, 30),
+            space = self.space ))
         springsList =[]
         springsCombination=[i for i in combinations(list(range(self.nodes)),2)]
         for _ in range(self.springs):
@@ -223,20 +234,23 @@ class subject():
             n1 = springsCombination[s1][0]
             n2 = springsCombination[s1][1]
             springsList.append(TSpringJoint( nodesList[n1].body, nodesList[n2].body,  
-            length = 20+random.randint(0,200),
-            stif=50+random.randint(0,50),
-            space = space,
-            grooved=random.randint(0,1)))
+            length = 200+random.randint(0,100),
+            stif = 100+random.randint(0,100),
+            space = self.space ,
+            grooved=1))
             springsCombination.remove(springsCombination[s1])
+        self.nodesList = nodesList
+        self.springList = springsList
+        self.positionStart = statistics.mean([x.body.position[0] for x in self.nodesList])
+        print(self.nodes , self.springs)
+
+    def fit(self):
+        self.positionEnd =  statistics.mean([x.body.position[0] for x in self.nodesList])
+        self.fitness = self.positionEnd - self.positionStart
+        return self.fitness
 
 
 if __name__ == '__main__':
-    size = w, h = 1800, 600
+    
     app = App(size)
-    space = app.space
-    b0 = space.static_body
-    subject = subject()
-    subject.create(space)
-    # print([x.rest_length for x in space.constraints])
- 
     app.run()
